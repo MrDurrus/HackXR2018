7abc789cbdcc39e173feac5096058d36e0b68cf6
tydaai <tydai@ucsd.edu>
2018-02-24 21:28:25 -0800

    World script

---
+++ Assets/OSPNative/scripts/World.cs
@@ -0,0 +1,1 @@
+﻿public class World : MonoBehaviour {  	// number of floating cubes 	int count;  	// Use this for initialization 	void Start () { 		// generates cubes 		for (; count < 5; count++) { 			new Piece (); 		} 	}  	// Update is called once per frame 	void Update () { 		// make sure there're always five cubes 		if (count < 5) { 			new Piece (); 			count++; 		} 	} }
