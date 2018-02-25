3bb2f2d16a39754af2c5cece60b0cdbe0a2b20f3
MrDurrus <mentojlin@gmail.com>
2018-02-24 20:43:53 -0800

    Update Piece.cs

--- Assets/OSPNative/scripts/Piece.cs
+++ Assets/OSPNative/scripts/Piece.cs
@@ -3,61 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // floating pieces 
-public class Piece : MonoBehaviour {
+public class Piece : MonoBehaviour
+{
 
-	float waitTime = 5f;
-	float timer;
-	GameObject marker;
-	Vector3 markerPos;
-	List<Vector3> platformPts, cubePts;
+    GameObject marker;
+    Vector3 markerPos;
+    List<Vector3> platformPts, cubePts;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(this.transform.parent ~= null) {
-			// disintegrates block five seconds after it's attached
-			timer += Time.deltaTime;
-			if (timer > waitTime) {
-				this.transform.parent = null;
-				Destroy (this);	
-			}
-		}
-	}
-	
-	// move floating piece using controller
-	void move(GameObject controller, GameObject platform) {
-		// distance between controller and the floating cube
-		Vector3 distance = controller.transform.position - this.transform.position;
-		this.transform.position = 2 * ((distance).normalized) + this.transform.position;
-	}	
 
-	// locks cube to platform
-	void lockCube(GameObject platform) {
-		// list of platform vertices 
-		Mesh platformMesh = platform.GetComponent<MeshFilter>().mesh;
-		Vector3[] platformPts = platformMesh.vertices;
-		// list of cube vertices
-		Mesh cubeMesh = this.GetComponent<MeshFilter> ().mesh;
-		Vector3[] cubePts = cubeMesh.vertices;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-		// iterate through vertices to find nearest side to connect
-		for (int i = 0; i < platformPts.Length; i++) {
-			for (int j = 0; j < cubePts.Length; j++) {
-				// distance between platform and cube
-				float distance = Vector3.Distance (platformPts [i], cubePts [j]);
-				// shows marker
-				if (distance < 10.0) {
-					GameObject marker = GameObject.CreatePrimitive (PrimitiveType.Cube);
-				}
-				// locks cube
-				if (distance < 2.0) {
-					this.transform.parent = platform.transform;
-					marker.transform.localPosition = Vector3.zero;
-				}
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update() { }
+
+
+    public void pullPiece(GameObject controller, GameObject platform) {
+        move(controller);
+        lockCube(platform);
+    }
+
+    // move floating piece using controller
+    public void move(GameObject controller)
+    {
+        // distance between controller and the floating cube
+        Vector3 distance = controller.transform.position  + 5*controller.transform.forward.normalized - this.transform.position;
+        if (distance.magnitude < 2) {
+            this.transform.position = controller.transform.position + 5 * controller.transform.forward.normalized;
+        } else {
+            this.transform.position = 0.7f * ((distance).normalized) + this.transform.position;
+        }
+    }
+
+    // locks cube to platform
+    void lockCube(GameObject platform)
+    {
+        // list of platform vertices 
+        Mesh platformMesh = platform.GetComponent<MeshFilter>().mesh;
+        Vector3[] platformPts = platformMesh.vertices;
+        // list of cube vertices
+        Mesh cubeMesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] cubePts = cubeMesh.vertices;
+
+        // iterate through vertices to find nearest side to connect
+        for (int i = 0; i < platformPts.Length; i++)
+        {
+            for (int j = 0; j < cubePts.Length; j++)
+            {
+                // distance between platform and cube
+                float distance = Vector3.Distance(platformPts[i], cubePts[j]);
+                // shows marker
+                if (distance < 20.0)
+                {
+                    GameObject marker = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                }
+                // locks cube
+                if (distance == 5.0 && !OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+                {
+                    this.transform.parent = platform.transform;
+                    marker.transform.localPosition = Vector3.zero;
+                }
+            }
+        }
+    }
 }
+
