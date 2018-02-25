ab21e720b82149bdd8a19e9f645188deeb008c5f
tydaai <30811405+tydaai@users.noreply.github.com>
2018-02-24 17:20:49 -0800

    Added lockCube()

--- Assets/OSPNative/scripts/Piece.cs
+++ Assets/OSPNative/scripts/Piece.cs
@@ -7,13 +7,11 @@ public class Piece : MonoBehaviour {
 
 	GameObject marker;
 	Vector3 markerPos;
+	List<Vector3> platformPts, cubePts;
 
 	// Use this for initialization
 	void Start () {
 	}
-	
-	// Update is called once per frame
-	void Update () {}
 
 	// move floating piece using controller
 	void move(GameObject controller, GameObject platform) {
@@ -21,5 +19,31 @@ public class Piece : MonoBehaviour {
 		Vector3 distance = controller.transform.position - this.transform.position;
 		this.transform.position = 2 * ((distance).normalized) + this.transform.position;
 	}	
-}
 
+	// locks cube to platform
+	void lockCube(GameObject platform) {
+		// list of platform vertices 
+		Mesh platformMesh = platform.GetComponent<MeshFilter>().mesh;
+		Vector3[] platformPts = platformMesh.vertices;
+		// list of cube vertices
+		Mesh cubeMesh = this.GetComponent<MeshFilter> ().mesh;
+		Vector3[] cubePts = cubeMesh.vertices;
+
+		// iterate through vertices to find nearest side to connect
+		for (int i = 0; i < platformPts.Length; i++) {
+			for (int j = 0; j < cubePts.Length; j++) {
+				// distance between platform and cube
+				float distance = Vector3.Distance (platformPts [i], cubePts [j]);
+				// shows marker
+				if (distance < 10.0) {
+					GameObject marker = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				}
+				// locks cube
+				if (distance < 2.0) {
+					this.transform.parent = platform.transform;
+					marker.transform.localPosition = Vector3.zero;
+				}
+			}
+		}
+	}
+}
