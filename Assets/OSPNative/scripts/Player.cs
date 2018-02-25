b81e4904ac01e09db36824194456775015d2b031
tydaai <30811405+tydaai@users.noreply.github.com>
2018-02-24 20:03:37 -0800

    Incorporated platform

--- Assets/OSPNative/scripts/Player.cs
+++ Assets/OSPNative/scripts/Player.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	GameObject platform;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start () {}
 	
 	// Update is called once per frame
-	void Update () {
-		// player's position
-		Vector3 playerPos = this.transform.position;
+	void Update () {}
+
+	// save reference for the platform
+	void onCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Cube") {
+			platform = col.gameObject;
+		}
 	}
 }
